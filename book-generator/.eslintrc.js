a1601bc7fb195259e173f72bc14e5c20c905f2ec
Michaël Perrin <contact@michaelperrin.fr>
2019-02-05 19:30:34 -0600

    Add ESLint validation

---
+++ book-generator/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": "off",
+    "react/prefer-stateless-function": "off",
+    "import/prefer-default-export": "off"
+  }
+};
